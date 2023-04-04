6fc174d396ad5956fa7cd009e393a18e83a4ffa4
Rodolfo Ferreira <rodolfo.lima.ferreira11@gmail.com>
2023-04-03 21:16:53 -0300

    ajuste imports

--- src/main/java/tech/ada/service/BonusService.java
+++ src/main/java/tech/ada/service/BonusService.java
@@ -1,5 +1,6 @@
 package tech.ada.service;
 
+import tech.ada.model.Cargo;
 import tech.ada.model.Funcionario;
 
 public class BonusService {
@@ -8,15 +9,17 @@ public class BonusService {
        Funcionário que vende acima de 20 roupas, recebem 5% de bônus
        Funcionário que vende acima de 30 roupas, recebem 10% de bônus
      */
-
     public double calcularBonus(Funcionario funcionario, int roupasVendidas) {
         double bonus = 0;
-        if (roupasVendidas > 20) {
-            bonus = 0.05;
-        }
 
-        if (roupasVendidas > 30) {
-            bonus = 0.10;
+        if (funcionario.getCargo().equals(Cargo.VENDEDOR)) {
+            if (roupasVendidas > 20) {
+                bonus = 0.05;
+            }
+
+            if (roupasVendidas > 30) {
+                bonus = 0.10;
+            }
         }
 
         return funcionario.getSalario() * bonus;
--- src/test/java/tech/ada/service/BonusServiceTest.java
+++ src/test/java/tech/ada/service/BonusServiceTest.java
@@ -1,8 +1,11 @@
 package tech.ada.service;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BonusServiceTest {
 
     //ESCREVAM OS TESTES AQUI
+
 }
